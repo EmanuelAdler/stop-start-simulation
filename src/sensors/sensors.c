1bc07b3d43a8d1f137a5ead4b602eab90f7c3182
p-r-m-n <paulo_rmn2010@hotmail.com>
2025-03-18 10:16:26 -0300

    Add initial sensor source code with data

---
+++ src/sensors/sensors.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <signal.h>
+
+/***** Speed sensor data *****/
+
+// constant to convert vehicle speed from rad/s to m/s
+float dois_pi_sobre_60 = 3.1415/30;
+
+// wheel radius, in meters
+float r_w  = 0.326;
+
+// 1/(differential ratio)
+float um_sobre_n_d  = 0.2817;
+
+// 1/(current gear ratio)
+float um_sobre_n_g_atual = 0.00;
+
+// current speed
+float vel_atual = 0;
+
+/***** Brake sensor data *****/
+
+int is_braking = 0;
+
+/***** Gear sensor data *****/
+
+// current gear
+int gear = 1;
+
+/***** Inclinometer sensor data *****/
+
+// current vehicle angle - horizontal reference
+float curr_vehicle_angle = 0;
+
+/***** Engine coolant sensor data *****/
+
+// Current engine coolant temperature
+float curr_cool_temp = 0;
+
+/***** Battery sensor data *****/
+
+// Current battery voltage
+float bat_voltage = 12;
+
+// Current battery State of Charge
+float bat_soc = 80;
+
+/***** AC sensor data *****/
+
+// Temperature outside the vehicle
+float out_temp = 28;
+
+// Temperature inside the vehicle
+float in_temp = 25;
+
+// Internal temperature reference
+float set_temp = 23;
+
+/***** Door sensor data *****/
+
+// Any door opened | 0 - no | 1 - yes |
+int opened_door = 0;
