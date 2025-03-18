21985880cb4de385727d88cf0b8f41458479a193
p-r-m-n <paulo_rmn2010@hotmail.com>
2025-03-18 17:26:43 -0300

    Added engine RPM calculation and formatted some data in the source. Also added the posix identifier in the clang tidy script

--- src/sensors/simu_sensors.c
+++ src/sensors/simu_sensors.c
@@ -1,75 +1,240 @@
 #include "simu_sensors.h"
 
+#define SLEEP_TIME_US           (100000U)
+#define MICROSECS_IN_ONESEC     (1000000L)
+#define NANO_TO_MICRO           (1000)
+
+void sleep_microseconds(long int msec) {
+    struct timespec tspec;
+    tspec.tv_sec = msec / MICROSECS_IN_ONESEC;
+    tspec.tv_nsec = (msec % MICROSECS_IN_ONESEC) * NANO_TO_MICRO;
+    nanosleep(&tspec, NULL);
+}
+
 /***** Speed sensor data *****/
 
-// constant to convert vehicle speed from rad/s to m/s
-#define PI_OVER_30 3.1415/30;
+/* Kilometers per hour to meters per second ratio */
+#define M_S_KM_H_RATIO        (3.6f)
 
-// wheel radius, in meters
-#define WHEEL_RADIUS 0.326;
+/* Constant to convert vehicle speed from rad/s to m/s */
+#define PI_OVER_30            (3.1415f / 30.0f)
 
-// 1/(differential ratio)
-#define ONE_OVER_PI 0.2817;
+/* Wheel radius, in meters */
+#define WHEEL_RADIUS          (0.326f)
 
-// 1/(current gear ratio)
-float um_sobre_n_g_atual = 0.00;
+/* 1/(differential ratio) */
+#define ONE_OVER_ND           (0.2817f)
 
-// current speed
-float vel_atual = 0;
+/* 1/(current gear ratio) */
+static float one_over_curr_ng = 0.0F;
+
+/* Current speed */
+static float vel_atual = 0.0F;
 
 /***** Brake sensor data *****/
 
-int is_braking = 0;
+static int is_braking = 0;
 
 /***** Gear sensor data *****/
 
-// current gear
-int gear = 1;
+#define MAX_RPM 5000
+
+#define NG_1    0.2611F
+#define NG_2    0.4237F
+#define NG_3    0.5917F
+#define NG_4    0.7634F
+#define NG_5    1.00F
+
+typedef enum {
+    GEAR_1 = 1,
+    GEAR_2,
+    GEAR_3,
+    GEAR_4,
+    GEAR_5,
+    GEAR_INVALID
+} Gear;
+
+/* Current gear */
+static Gear gear = GEAR_1;
+
+/* Current engine RPM */
+static int curr_RPM = 0;
+
+/* Current gear variation step */
+static int curr_step = 1;
+
+/* Number of simulated steps which cause gear to change */
+static const int num_sim_steps = 5;
+
+/* RPM loss when changing gear */
+static const int RPM_gear_loss = 1200;
+
+/* RPM increment step when accelerating */
+static const int RPM_increment_step = 300;
+
+/* RPM decrement step when braking */
+static const int RPM_decrement_step = 450;
 
 /***** Inclinometer sensor data *****/
 
-// current vehicle angle - horizontal reference
-float curr_vehicle_angle = 0;
+/* Current vehicle angle - horizontal reference */
+static float curr_vehicle_angle = 0.0F;
 
 /***** Engine coolant sensor data *****/
 
-#define ACCEL_FACTOR 0.1
-#define AIR_COOL_FACTOR 0.05
-#define MAX_TEMP_MOTOR 140
-#define BASE_TEMP 80
+#define ACCEL_FACTOR          (0.1f)
+#define AIR_COOL_FACTOR       (0.05f)
+#define MAX_TEMP_MOTOR        (105.0f)
+#define BASE_TEMP             (70.0f)
 
-// Current engine coolant temperature
-float curr_cool_temp = 0;
+#define TEMP_MOD    (1 / 10.0f)
+
+/* Current engine coolant temperature */
+static float curr_cool_temp = 0.0F;
 
 /***** Battery sensor data *****/
 
-// Current battery voltage
-float bat_voltage = 12;
+#define DEFAULT_BATTERY_VOLTAGE     12.0F
+#define DEFAULT_BATTERY_SOC        80.0F
 
-// Current battery State of Charge
-float bat_soc = 80;
+static float bat_voltage = DEFAULT_BATTERY_VOLTAGE;
+static float bat_soc = DEFAULT_BATTERY_SOC;
 
 /***** AC sensor data *****/
 
-// Temperature outside the vehicle
-float out_temp = 28;
+#define DEFAULT_OUTSIDE_TEMP       28.0F
+#define DEFAULT_INSIDE_TEMP        25.0F
+#define DEFAULT_SET_TEMP           23.0F
 
-// Temperature inside the vehicle
-float in_temp = 25;
-
-// Internal temperature reference
-float set_temp = 23;
+static float out_temp = DEFAULT_OUTSIDE_TEMP;
+static float in_temp = DEFAULT_INSIDE_TEMP;
+static float set_temp = DEFAULT_SET_TEMP;
 
 /***** Door sensor data *****/
 
-// Any door opened | 0 - no | 1 - yes |
-int opened_door = 0;
+/* Any door opened | false - no | true - yes | */
+static bool opened_door = false;
 
 /***** Calculate engine temperature *****/
 
-float calculate_engine_temp(float velocidade, int rpm) {
-    float temp_rise = rpm/10 * ACCEL_FACTOR;
-    float cooling_effect = velocidade * AIR_COOL_FACTOR;
+static float calculate_engine_temp(struct engine_temp etemp) 
+{
+    float temp_rise = ((float)etemp.engine_rpm * TEMP_MOD * ACCEL_FACTOR);
+    float cooling_effect = etemp.vehicle_speed * AIR_COOL_FACTOR;
     float temp = BASE_TEMP + temp_rise - cooling_effect;
     return fminf(MAX_TEMP_MOTOR, temp);
+}
+
+/***** Calculate speed *****/
+
+static void *sensor_speed(void *arg) 
+{
+    float *speed = (float *)arg;
+
+    while (true) 
+    {
+        int lock_result = pthread_mutex_lock(&mutex_sensors);
+        if (lock_result != 0) 
+        {
+            /* Handle error - example: log and exit */
+            return NULL;
+        }
+
+        switch (gear) 
+        {
+            case GEAR_1:
+                one_over_curr_ng = NG_1;
+                break;
+            case GEAR_2:
+                one_over_curr_ng = NG_2;
+                break;
+            case GEAR_3:
+                one_over_curr_ng = NG_3;
+                break;
+            case GEAR_4:
+                one_over_curr_ng = NG_4;
+                break;
+            case GEAR_5:
+                one_over_curr_ng = NG_5;
+                break;
+            default:
+                /* Handle unexpected gear value */
+                break;
+        }
+
+        *speed = ((float)curr_RPM * WHEEL_RADIUS * one_over_curr_ng * 
+                 ONE_OVER_ND * PI_OVER_30 * M_S_KM_H_RATIO);
+
+        int unlock_result = pthread_mutex_unlock(&mutex_sensors);
+        if (unlock_result != 0) 
+        {
+            /* Handle error */
+            return NULL;
+        }
+
+        sleep_microseconds(SLEEP_TIME_US);
+    }
+    return NULL;
+}
+
+/***** Calculate RPM *****/
+
+static void *sensor_gear_pos(void *arg) 
+{
+    int *rpm = (int *)arg;
+
+    while (true) 
+    {
+        int lock_result = pthread_mutex_lock(&mutex_sensors);
+        if (lock_result != 0) 
+        {
+            return NULL;
+        }
+
+        if (curr_step == num_sim_steps) 
+        {
+            if (is_braking) 
+            {
+                if (gear > GEAR_1) 
+                {
+                    gear--;
+                }
+            } 
+            else 
+            {
+                if (gear < GEAR_5) 
+                {
+                    gear++;
+                }
+            }
+            curr_step = 1;
+        } 
+        else 
+        {
+            curr_step++;
+        }
+
+        int delta = is_braking ? -RPM_decrement_step : RPM_increment_step;
+        curr_RPM += delta;
+
+        if (curr_RPM > MAX_RPM) 
+        {
+            curr_RPM = MAX_RPM;
+        } 
+        else if (curr_RPM < 0) 
+        {
+            curr_RPM = 0;
+        }
+
+        *rpm = curr_RPM;
+
+        int unlock_result = pthread_mutex_unlock(&mutex_sensors);
+        if (unlock_result != 0) 
+        {
+            return NULL;
+        }
+
+        sleep_microseconds(SLEEP_TIME_US);
+    }
+    return NULL;
 }
--- src/sensors/simu_sensors.h
+++ src/sensors/simu_sensors.h
@@ -1,6 +1,7 @@
 #ifndef SIMU_SENSORS_H
 #define SIMU_SENSORS_H
 
+#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
@@ -8,7 +9,13 @@
 #include <pthread.h>
 #include <signal.h>
 #include <math.h>
+#include <stdbool.h>
 
 extern pthread_mutex_t mutex_sensors;
+
+struct engine_temp{
+    int engine_rpm;
+    float vehicle_speed;
+};
 
 #endif // SIMU_SENSORS_H
